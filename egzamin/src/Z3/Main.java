a2222d84126d004888e0836d5a88e1caf46f87a7
maciejszulia <maciej.szulia@gmail.com>
2022-02-14 12:14:36 +0100

    egzamin2

--- egzamin/src/Z3/Main.java
+++ egzamin/src/Z3/Main.java
@@ -1,5 +1,18 @@
 package Z3;
 
-public class Main {
+import Z1.ArrayUtil;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        File katalog_1 = new File("C:/Program Files");
+        File[] listaKatalogow = Funkcja.wszystkiePodkatalogi(katalog_1);
+        ArrayList<File>listaKatalogow_1 = new ArrayList<>(List.of(listaKatalogow));
+
+        System.out.println(listaKatalogow);
+        System.out.println(listaKatalogow_1);
+    }
 }
